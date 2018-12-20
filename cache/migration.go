93d17732e5b9536c8c8c6d7ba8680e6e7c4fc5ed
soft as HELL <softashell@cock.li>
2018-12-20 20:35:33 +0200

    Fix bolt import

--- cache/migration.go
+++ cache/migration.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"time"
 
-	"go.etcd.io/bbolt"
-
 	log "github.com/Sirupsen/logrus"
+	"go.etcd.io/bbolt"
 )
 
 type oldCacheItem struct {
@@ -77,7 +76,7 @@ func (c *Cache) migration1() error {
 
 		items := []Item{}
 
-		err := c.db.Bolt.Update(func(tx *bbolt.Tx) error {
+		err := c.db.Bolt.Update(func(tx *bolt.Tx) error {
 			b := tx.Bucket([]byte(bucketName))
 			if b == nil {
 				return fmt.Errorf("bucket doesn't exist: %s", string(bucketName))
@@ -160,7 +159,7 @@ func (c *Cache) migration1() error {
 	}
 
 	// Delete old metadata
-	err := c.db.Bolt.Update(func(tx *bbolt.Tx) error {
+	err := c.db.Bolt.Update(func(tx *bolt.Tx) error {
 		err := tx.DeleteBucket([]byte("___metadata"))
 		if err != nil {
 			log.Error(err)
